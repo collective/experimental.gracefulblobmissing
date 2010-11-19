dca7b77349d044fc9e9fc60a52b752e796187b5d
keul <keul@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-19 16:05:12 +0000

    First commit, let's test this
    git-svn-id: http://svn.plone.org/svn/collective/experimental.gracefulblobmissing/trunk@227097 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ experimental/gracefulblobmissing/patches.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from os import fstat
+from ZODB.POSException import POSKeyError
+from plone.app.blob.utils import openBlob
+
+def patched_field_get_size(self):
+    try:
+        blob = openBlob(self.blob)
+        size = fstat(blob.fileno()).st_size
+        blob.close()
+    except POSKeyError:
+        size = 0
+    return size
+
+def get_size(self):
+    f = self.getPrimaryField()
+    if f is None:
+        return 0
+    try:
+        return f.get_size(self) or 0
+    except POSKeyError: 
+        return 0
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='experimental.gracefulblobmissing',
+      version=version,
+      description="Patch Plone to not brake when visiting file like contents where related BLOB is missing",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        ],
+      keywords='plone blob patch',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['experimental'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.monkeypatcher'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
