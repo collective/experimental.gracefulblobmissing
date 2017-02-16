02476a31ae078d73063f24238e4d505940f223d7
Alessandro Pisa <alessandro.pisa@gmail.com>
2017-02-16 15:44:50 +0100

    pep8

--- experimental/gracefulblobmissing/patches.py
+++ experimental/gracefulblobmissing/patches.py
@@ -9,11 +9,14 @@ from Products.CMFCore.utils import getToolByName
 from sh import mkdir
 from ZEO import ClientStorage
 from ZODB.blob import BlobFile
-from ZODB.POSException import POSKeyError, ConflictError, Unsupported
+from ZODB.POSException import ConflictError
+from ZODB.POSException import POSKeyError
+from ZODB.POSException import Unsupported
 
 import logging
 import os
 import time
+
 
 logger = logging.getLogger(__name__)
 
@@ -38,9 +41,20 @@ def patched_class_get_size(self):
         return 0
 
 
-def patched_field_index_html(self, instance, REQUEST=None, RESPONSE=None, disposition='inline'):
+def patched_field_index_html(
+    self,
+    instance,
+    REQUEST=None,
+    RESPONSE=None,
+    disposition='inline',
+):
     try:
-        blob = self._old_index_html(instance, REQUEST=REQUEST, RESPONSE=RESPONSE, disposition=disposition)
+        blob = self._old_index_html(
+            instance,
+            REQUEST=REQUEST,
+            RESPONSE=RESPONSE,
+            disposition=disposition,
+        )
         if blob:
             return blob
         raise POSKeyError()
@@ -48,7 +62,8 @@ def patched_field_index_html(self, instance, REQUEST=None, RESPONSE=None, dispos
         if not RESPONSE:
             RESPONSE = instance.REQUEST.RESPONSE
         putils = getToolByName(instance, 'plone_utils')
-        putils.addPortalMessage('Missing BLOB file for %s' % instance.absolute_url_path(), type='warning')
+        putils.addPortalMessage('Missing BLOB file for %s' %
+                                instance.absolute_url_path(), type='warning')
         RESPONSE.redirect(instance.absolute_url() + '/view')
 
 
@@ -88,7 +103,12 @@ def patched_SearchableText(self):
             vocab = field.Vocabulary(self)
             if isinstance(datum, list) or isinstance(datum, tuple):
                 # Unmangle vocabulary: we index key AND value
-                vocab_values = map(lambda value, vocab=vocab: vocab.getValue(value, ''), datum)
+                vocab_values = map(
+                    lambda value, vocab=vocab: (
+                        vocab.getValue(value, ''),
+                        datum
+                    )
+                )
                 datum = list(datum)
                 datum.extend(vocab_values)
                 datum = ' '.join(datum)
--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
+
 import os
 
-version = '0.5.1.dev0'
 
-setup(name='experimental.gracefulblobmissing',
-      version=version,
-      description="Patch for Plone. Don't raise errors for file contents with missing BLOB file",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "CHANGES.rst")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+version = "0.5.1.dev0"
+
+setup(
+    name="experimental.gracefulblobmissing",
+    version=version,
+    description=(
+        "Patch for Plone. "
+        "Don't raise errors for file contents with missing BLOB file"
+    ),
+    long_description=open("README.rst").read()
+    + "\n"
+    + open(os.path.join("docs", "CHANGES.rst")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.1",
@@ -21,24 +29,24 @@ setup(name='experimental.gracefulblobmissing',
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        ],
-      keywords='plone blob patch',
-      author='RedTurtle Technology',
-      author_email='sviluppoplone@redturtle.it',
-      url='http://plone.org/products/experimental.gracefulblobmissing',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['experimental'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'collective.monkeypatcher>=1.0',
-          'sh'
-      ],
-      entry_points="""
+    ],
+    keywords="plone blob patch",
+    author="RedTurtle Technology",
+    author_email="sviluppoplone@redturtle.it",
+    url="http://plone.org/products/experimental.gracefulblobmissing",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["experimental"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "collective.monkeypatcher>=1.0",
+        "sh",
+    ],
+    entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
